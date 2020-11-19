b8d1a435ed95d395738cb809e8eacaf4db41b439
Nick <i@httpd.cc>
2020-11-19 11:03:17 +0800

    增加注释说明

--- src/YapiDTO.php
+++ src/YapiDTO.php
@@ -8,27 +8,40 @@ use Illuminate\Support\Arr;
 /**
  * Class YapiDTO
  * @package Cblink\YApiDoc
+ * @property-read string $project
  * @property-read string $name
  * @property-read string $desc
  * @property-read string $category
  * @property-read array $params
- * @property-read array $project
+ * @property-read array $request
+ * @property-read array $response
  */
 class YapiDTO extends DTO
 {
     public function rules(): array
     {
         return [
+            // 项目名
             'project' => ['nullable', 'array'],
+            // 接口名
             'name' => ['required', 'string'],
+            // 路由参数
             'params' => ['nullable', 'array'],
+            // 接口分类
             'category' => ['nullable', 'string'],
+            // 接口简介
             'desc' => ['nullable', 'string'],
+            // 请求参数
             'request' => ['nullable', 'array'],
+            // 请求参数说明
             'request.trans' => ['nullable' , 'array'],
+            // 请求参数排除必填
             'request.except' => ['nullable', 'array'],
+            // 返回参数
             'response' => ['nullable', 'array'],
+            // 返回参数说明
             'response.trans' => ['nullable' , 'array'],
+            // 返回参数排除必填
             'response.except' => ['nullable', 'array'],
         ];
     }
