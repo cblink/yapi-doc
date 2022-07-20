eb3f6ff52b76f8de827e373ae0f9e0d66057c446
mouyong <mouyong@users.noreply.github.com>
2022-07-20 09:48:41 +0800

    调整配置文件中英文数字风格

--- config/yapi.php
+++ config/yapi.php
@@ -1,38 +1,41 @@
 <?php
 
+/**
+ * sync with https://github.com/cblink/yapi-doc/blob/master/config/yapi.php
+ */
 return [
     // yap请求地址
-    'base_url' => 'http://xxxxxxxx/',
+    // 'base_url' => 'http://xxxxxxxx/', // custom yapi url
+    // 'base_url' => 'http://yapi.smart-xwork.cn/', // yapi url
+    'base_url' => false, // disable upload to yapi
     // 文档合并方式，"normal"(普通模式) , "good"(智能合并), "merge"(完全覆盖)
     'merge' => 'merge',
 
     'config' => [
         'default' => [
             'id' => 1,
-            'token' => ''
-        ],
+            'token' => '',
+        ]
     ],
 
     'openapi' => [
-        'enable' => false,
+        'enable' => true, // generate openapi.json
         'path' => public_path('openapi.json'),
     ],
 
     'public' => [
-        // 前缀
         'prefix' => 'data',
 
         // 公共的请求参数,query部分
         'query' => [
-            'page' => ['plan' => '页码，默认1'],
-            'page_size' => ['plan' => '每页数量，不超过200，默认15'],
-            'is_all' => ['plan' => '是否获取全部数据，不超过1000条'],
+            'page' => ['plan' => '页码，默认 1'],
+            'per_page' => ['plan' => '每页数量，不超过 200，默认 15'],
         ],
 
         // 公共的响应参数
         'data' => [
-            'err_code' => ['plan' => '错误码，0表示成功', 'must' => true, 'required' => true],
-            'err_msg' => ['plan' => '错误说明，请求失败时范湖', 'must' => true],
+            'err_code' => ['plan' => '错误码，200 表示成功', 'must' => true, 'required' => true],
+            'err_msg' => ['plan' => '错误说明，请求失败时返回', 'must' => true],
             'meta' => [
                 'plan' => '分页数据',
                 'must' => false,
